e03eb467209d1b30b468c873717b1cc9788f8967
mgsky1 <563044462@qq.com>
2024-12-11 05:49:59 +0000

    Deploying to gh-pages from @ mgsky1/memory-gallery@bccf14222482ce38f152c1790746a0429e0c1aea 🚀

--- constants/timeline.ts
+++
@@ -1,8 +0,0 @@
-import type { TimeEvent } from '~/types'
-
-export const timelineEvents: TimeEvent[] = [
-  { id: 1, year: 2024, season: '春' },
-  { id: 2, year: 2024, season: '夏' },
-  { id: 3, year: 2024, season: '秋' },
-  { id: 4, year: 2024, season: '冬' }
-] 
--- types/shims-json.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '*.json' {
-  const value: {
-    photos: import('./index').Photo[]
-  }
-  export default value
-} 
