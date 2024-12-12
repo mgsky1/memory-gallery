1b02fe6383faa76c8ebe948c494cdb9e4aaea536
Martin Huang <563044462@qq.com>
2024-12-12 17:28:48 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,5 +7,18 @@ export default defineNuxtConfig({
     exposeConfig: false,
     injectPosition: 0,
     viewer: true,
+  },
+  // 确保生产构建时资源路径正确
+   app: {
+    baseURL: '/dist',
+    buildAssetsDir: 'nuxt/', // 默认值
+    cdnURL: '' // 如果需要CDN可以在这里配置
+  },
+  
+  // 确保正确的构建输出
+  nitro: {
+    output: {
+      publicDir: 'dist'
+    }
   }
 }) 
